a094c1b6762d2b96ddd2e25edbe93310ce062ac8
Markus Meingast <markus.meingast@mail.mcgill.ca>
2019-10-05 12:00:06 +0200

    started quickplot script for determining events and noise

---
+++ 02_Pre/02_development/04_quick_plot.py
@@ -0,0 +1,18 @@
+#!/home/pi/Software/python3.7/bin/python3
+
+import pandas as pd
+import glob
+import sys
+import matplotlib.pylab as mp
+
+csvs = glob.glob('../03_samples/03_samples_peak_to_peak_streched/20191004-222339/*.csv')
+
+data = pd.DataFrame()
+
+for i in range(len(csvs)):
+	data = data.append(pd.read_csv(csvs[i],header=None).T,ignore_index=True)
+
+data.T.plot()
+mp.show()
+
+
